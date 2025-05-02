e735fd077223d358c4dec00234848e4e020d6c5d
enesozsahin <117269312+enesozsahin@users.noreply.github.com>
2025-05-02 20:00:50 +0200

    Initial commit

---
+++ catalog/admin.py
@@ -0,0 +1,71 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+from .models import Author, Genre, Book, BookInstance, Language
+
+#admin.site.register(Book)
+#admin.site.register(Author)
+#admin.site.register(BookInstance)
+admin.site.register(Genre)
+#admin.site.register(Language)
+
+
+
+
+class BooksInstanceInline(admin.StackedInline):
+    model = BookInstance
+    extra= 0
+
+
+class BookInline(admin.StackedInline):
+    model = Book
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    
+    #Play with these modify them
+
+    fieldsets = (
+        ('General Info', {
+            'fields': ('first_name', 'last_name','genre')
+        }),
+        ('Dates', {
+            'fields': (('date_of_birth', 'date_of_death'))
+        }),
+    )
+
+
+
+
+
+
+
+
+ #Compare the view of __bookinstance__ with and without
+#> `inlines = [BooksInstanceInline]
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', Author, 'display_genre')
+
+    inlines = [BooksInstanceInline]    
+
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+
+#Added language and inline book
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+    inlines =[BookInline]
