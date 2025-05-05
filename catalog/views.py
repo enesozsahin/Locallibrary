c9f9045acac6fa1410509959ad024097c6736d01
enesozsahin <117269312+enesozsahin@users.noreply.github.com>
2025-05-05 13:43:44 +0200

    New update of sessions
    New version added sessions.

--- catalog/views.py
+++ catalog/views.py
@@ -6,9 +6,14 @@ from .models import Book, Author, BookInstance, Genre
 def index(request):
     """View function for home page of site."""
 
+
+    
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
@@ -16,15 +21,20 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
-    # The nonsense
-    myStr = "nonsense"
+    request.session.set_test_cookie()
+    if request.session.test_cookie_worked():
+        request.session.delete_test_cookie()
+        num_visits = request.session.get('num_visits', 0)
+        request.session['num_visits'] = num_visits + 1
+    else:
+        num_visits = -1
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'Nonsense': myStr,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
